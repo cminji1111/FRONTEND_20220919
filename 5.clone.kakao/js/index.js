9377dcb699caa9994ade2178d5f669f9cd5ad8de
cminji1111 <cminji11@gmail.com>
2022-10-26 22:21:35 +0900

    20221026

--- 5.clone.kakao/js/index.js
+++ 5.clone.kakao/js/index.js
@@ -1,4 +1,8 @@
 let headerElem = document.getElementById('header')
+let navItemList = document.querySelectorAll('#gnbList li a')
+let gnbListElem = document.getElementById('gnbList')
+
+console.log(navItemList)
 
 let currScroll = window.scrollY
 console.log(currScroll)
@@ -22,3 +26,54 @@ window.addEventListener('scroll', (e) => {
 //     if (e.deltaY > 0) console.log('내림')
 //     else console.log('올림')
 // })
+
+// navItemList.forEach((item) => {
+//   item.addEventListener('mouseenter', () => {
+//     navItemList.forEach((depth) => {
+//       if (item === depth) depth.classList.add('active')
+//       else depth.classList.remove('active')
+//     })
+//   })
+// })
+
+// item.addEventListener(('mouseleave', () => {
+//   navItemList.forEach((depth) => {
+//     depth.classList.add('active');
+//   });
+// });
+
+gnbListElem.addEventListener('mouseover', function (e) {
+  /* 
+  e.target : 이벤트가 발생한 요소
+  e.currentTarget : 이벤트가 바인딩된 요소
+*/
+  if (e.target.matches('#gnbList > li > a')) {
+    navItemList.forEach((item) => {
+      if (e.target === item) item.classList.add('active')
+      else item.classList.remove('active')
+    })
+  }
+})
+
+gnbListElem.addEventListener('mouseleave', function () {
+  navItemList.forEach((item) => {
+    item.classList.add('active')
+  })
+})
+
+gnbListElem.addEventListener('click', function (e) {
+  // depth01 클릭시에만 동작.
+  if (e.target.matches('#gnbList > li > a')) {
+    // a 태그 클릭 시 상위 노트(li) 안에 .depth02 요소 있는지 확인
+    if (e.target.parentNode.querySelector('.depth02')) {
+      // null, undefined는 false처럼 작동한다.
+      e.proventDefault() // 요소의 기본 기능을 실행하지 않는다.
+      let depth01List = gnbListElem.querySelectorAll('#gnbList li')
+      depth01List.forEach((depth) => {
+        depth.classList.remove('on')
+      })
+      // 내가 클릭 li
+      e.target.parentNode.classList.add('on')
+    }
+  }
+})
